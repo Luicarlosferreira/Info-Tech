1bce57c7cf654e458331789ebb61d220c56b1b29
Luis Carlos Ferreira <77106308+Luicarlosferreira@users.noreply.github.com>
2022-06-24 16:42:02 +0100

    update project build

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -1,64 +1,57 @@
-import { useParams } from "react-router"
-import { Container } from './styles'
-import {useFetch} from '../../hooks/useFetch'
-import { FaOpencart, FaAngleRight } from 'react-icons/fa'
-import { Link } from "react-router-dom"
+import { useParams } from "react-router";
+import { Container } from "./styles";
+import { useFetch } from "../../hooks/useFetch";
+import { FaOpencart, FaAngleRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export const Product = () => {
- 
   const { id } = useParams();
   const url = "http://localhost:3000/product/" + id;
-  
-    const {data: item, loading, error } = useFetch(url)
-  
+
+  const { data: item, loading, error } = useFetch(url);
 
   return (
-    <Container>    
-      <div className="categoryContent">{item && 
-      (<>
+    <Container>
+      <div className="categoryContent">
+        {item && (
+          <>
+            <Link to="/products">
+              <p>Products</p>
+            </Link>
+            <FaAngleRight />
+            <p>{item.category}</p>
+            <FaAngleRight />
+          </>
+        )}
+      </div>
+      <div id="productsCard" className="card">
+        {loading && <p>Carregando...</p>}
+        {error && <p>{error}</p>}
 
+        {item && (
+          <div className="detailsContent">
+            <div className="cardImage">
+              <img src={item.image} />
+            </div>
 
-      <Link to="/products"><p>Products</p></Link> 
-       <FaAngleRight />
-       <p>{item.category}</p>
-       <FaAngleRight />  
+            <div className="detailsItem">
+              <h1>{item.name}</h1>
+              <p>{item.title}</p>
 
-
-      </>)}</div>
-     <div id='productsCard' className="card" >          
-           
-            {loading && (<p>Carregando...</p>)}
-            {error && (<p>{error}</p>)} 
-
-            {item && (
-               <div className="detailsContent">           
-                  <div className="cardImage"><img  src={item.image}/></div>
-                
-
-
-                  <div className="detailsItem">
-                    <h1>{item.name}</h1>
-                  <p>{item.title}</p>  
-                        
-                  <h2> $ {item.price}</h2>
-                  <div className="buyOption">
-                    <button type="submit"><FaOpencart /> Add to cart</button>
-                    <button type="submit"> Buy</button>
-                    
-                  </div>
-                  <div className="moreInformation">
-                    <p>More information</p>
-                  </div>
-                  
-
-                  </div>
-                  
-            
+              <h2> $ {item.price}</h2>
+              <div className="buyOption">
+                <button type="submit">
+                  <FaOpencart /> Add to cart
+                </button>
+                <button type="submit"> Buy</button>
               </div>
-            )}          
-                    
-           </div>     
-    
+              <div className="moreInformation">
+                <p>More information</p>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </Container>
-  )
-}
+  );
+};
